b7bfc2a2d0df51aa9c6e42c87d44723f40d47822
Will <dexterarcher@gmail.com>
2021-10-31 02:11:58 -0400

    updated Nav

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Header from '../components/Header';
-import Nav from '../components/nav';
+import Nav from '../components/Nav';
 import Results from '../components/Results';
 import requests from '../utils/requests'
 // import Image from 'next/image'
